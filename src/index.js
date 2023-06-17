4becd0b1a8c644702a14834c33294dd8064d2785
Wennuis <Wennuisa@gmail.com>
2023-06-17 14:50:39 -0400

    Modificamos el index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import { saludar } from "./js/componentes";
 import { history } from "./js/componentes";
 import './styles.css';
 
-const nombre = 'Wennuis adfasdf';
+const nombre = 'Webpack Prueba';
 saludar( nombre );
 
-const historia = ' Thiari Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.';
+const historia = 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.';
 history( historia );
